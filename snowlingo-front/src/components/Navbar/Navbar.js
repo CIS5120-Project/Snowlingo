08c1911554ce7c4d57ce0133acdb81930bbb4625
zhenyuan wang <zywangdylan@gmail.com>
2024-03-28 17:29:22 -0400

    navbar styled

--- snowlingo-front/src/components/Navbar/Navbar.js
+++ snowlingo-front/src/components/Navbar/Navbar.js
@@ -1,13 +1,44 @@
 import React from "react";
+import { AppBar, Toolbar, IconButton, Typography, useTheme } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import HelpIcon from '@mui/icons-material/Help';
+import HomeIcon from '@mui/icons-material/Home';
 
 function Navbar (){
+  const theme = useTheme();
   return (
-    <div>
-      <main> Navbar </main>
-      <footer>
-        <p>© 2024 Snowlingo. All rights reserved.</p>
-      </footer>
-    </div>
+    <AppBar position="static" style={{ width: '100%', padding: '0 1rem' }} sx={{ backgroundColor: theme.palette.primary.dark }}>
+      <Toolbar>
+        {/* Burger Menu Icon */}
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+
+        {/* Spacer to push icons to the right */}
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} />
+
+        {/* Help Icon */}
+        <IconButton
+          color="inherit"
+          aria-label="help"
+        >
+          <HelpIcon />
+        </IconButton>
+
+        {/* Home Icon */}
+        <IconButton
+          color="inherit"
+          aria-label="home"
+        >
+          <HomeIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
   );
 };
 
