f5bcb00a541dcd2e5f755b54dc1bfc045a962aa1
xunmiw17 <frwu@seas.upenn.edu>
2024-03-28 18:46:43 -0400

    PersonInfo content + simple style

--- snowlingo-front/src/pages/PersonInfo/PersonInfo.js
+++ snowlingo-front/src/pages/PersonInfo/PersonInfo.js
@@ -4,7 +4,28 @@ function PersonInfo (){
   return (
     <div>
       <main> PersonInfo </main>
-      <h1>About me</h1>
+      <h1>About you</h1>
+      <div>
+        <h2>You want to learn ...</h2>
+        <button>
+          Snowboard
+        </button>
+        <button>
+          Ski
+        </button>
+      </div>
+      <div>
+        <h2>I am ...</h2>
+        <button>
+          Beginner
+        </button>
+        <button>
+          Intermediate
+        </button>
+        <button>
+          Advanced
+        </button>
+      </div>
       <footer>
         <p>© 2024 Snowlingo. All rights reserved.</p>
       </footer>
