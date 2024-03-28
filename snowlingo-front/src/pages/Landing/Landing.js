e03827ccc7fe6ca34de7d89c0092472431d71caf
lucawu <lucawu0725@gmail.com>
2024-03-28 19:28:05 -0400

    landing page

--- snowlingo-front/src/pages/Landing/Landing.js
+++ snowlingo-front/src/pages/Landing/Landing.js
@@ -1,11 +1,24 @@
 import "./Landing.css"
-import React from "react";
-import { Container, Divider, Link } from '@mui/material';
+import React,{useState} from "react";
+import { useNavigate } from 'react-router-dom';
+import { Container, Divider, Link, IconButton } from '@mui/material';
 import { TextareaAutosize } from '@mui/base';
 import snow from './snowlingo.svg';
 import PhoneIcon from '@mui/icons-material/Phone';
 
 function Landing (){
+  let navigate = useNavigate();
+  const goToLogin =() => {
+    navigate('/Login');
+  }
+  const goToPersoninfo =() => {
+    navigate('/Personalinfo');
+  }
+
+  const handleLoginClick = (event) => {
+    
+  }
+
   return (
     <div>
       <main> Landing </main>
@@ -15,19 +28,19 @@ function Landing (){
       <p class="advanture">Great Advanture!</p>
       
       <div> 
-        <p class='login'>Login {'>>>'}</p>
+        <IconButton class='login' onClick={goToLogin}>Login {'>>>'}</IconButton>
         <div class='account-sign'>
             <p style={{marginTop:'0px'}}>Don't have account?  </p>
-            <p class='signup'> SignUp here</p>
+            <p class='signup' onClick={goToPersoninfo}> SignUp here</p>
         </div>
       </div>
-      <div style={{ marginTop: '400px', marginBottom: '100px' }}>
-        <PhoneIcon>Contact us!</PhoneIcon>
-        <p>Contact us!</p>
+      <div style={{ marginTop: '400px', marginBottom: '100px', display: 'flex'}}>
+        <PhoneIcon style={{color: 'white'}}></PhoneIcon>
+        <p class='contact'>Contact us!</p>
       </div>
 
       <footer>
-        <p>© 2024 Snowlingo. All rights reserved.</p>
+        <p style={{color: 'white'}}>© 2024 Snowlingo. All rights reserved.</p>
       </footer>
     </div>
   );
