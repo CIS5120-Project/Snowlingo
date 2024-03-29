2e70568727177092d0bcd959e53267ee45aca66a
xunmiw17 <frwu@seas.upenn.edu>
2024-03-29 10:56:30 -0400

    Reimpl PersonInfo in material UI

--- snowlingo-front/src/pages/PersonInfo/PersonInfo.js
+++ snowlingo-front/src/pages/PersonInfo/PersonInfo.js
@@ -2,35 +2,199 @@ import React from "react";
 import "./PersonInfo.css";
 import logo from "../../snowlingo.svg"
 
+import { useNavigate } from 'react-router-dom';
+import { Box, Button, Stack, Typography, useTheme } from '@mui/material';
+import CreateIcon from '@mui/icons-material/Create';
+
 function PersonInfo () {
+  const theme = useTheme();
+  const navigate = useNavigate();
+
   return (
-    <div className="personInfoContainer">
-      <main> PersonInfo </main>
-      <div style={{margin: '10% 10%'}}>
-        <h1>About you</h1>
-        <div>
-          <h2>You want to learn ...</h2>
-          <button>
-            Snowboard
-          </button>
-          <button>
-            Ski
-          </button>
-        </div>
-        <div>
-          <h2>I am ...</h2>
-          <button>
-            Beginner
-          </button>
-          <button>
-            Intermediate
-          </button>
-          <button>
-            Advanced
-          </button>
-        </div>
-      </div>
-    </div>
+    <Box
+      sx={{
+        margin: "10% 10%",
+        // display: 'flex',
+        // flexDirection: 'column',
+        // alignItems: 'center',
+        // gap: 2,
+        // margin: '3rem 1.5rem'
+      }}>
+      <img src={logo} alt="logo img" style={{ marginTop: "1rem", marginBottom: "1.5rem" }}/>
+      <Typography
+        variant="h4"
+        gutterBottom
+        sx={{
+          color: theme.palette.primary.light
+        }}
+      >
+        About you
+      </Typography>
+      <Box
+        sx={{
+          marginTop: '2rem',
+          marginBottom: '2rem'
+        }}>
+        <Typography
+          variant="body3"
+          gutterBottom
+          sx={{
+            color: theme.palette.primary.light
+          }}
+        >
+          You want to learn ...
+        </Typography>
+        <Stack direction="column" spacing={2} sx={{ width: '100%', maxWidth: 360, mt: 3 }}>
+          <Button
+            variant="outlined"
+            fullWidth
+            sx={{
+              height: '7vh',
+              backgroundColor: theme.palette.primary.dark,
+              '&:hover': { backgroundColor: theme.palette.secondary.main},
+              borderColor: theme.palette.secondary.main,
+              padding: '8px 30px',
+              paddingRight: '16px',
+              borderRadius: '15px',
+              color: theme.palette.primary.main,
+              justifyContent: 'center'
+            }}
+            onClick = {() => navigate("/maintenance")}
+          >
+            <Typography
+              variant="body3"
+              gutterBottom
+              sx={{
+                color: theme.palette.primary.light
+              }}
+            >
+              Snowboard
+            </Typography>
+          </Button>
+          <Button
+            variant="outlined"
+            fullWidth
+            sx={{
+              height: '7vh',
+              backgroundColor: theme.palette.primary.dark,
+              '&:hover': { backgroundColor: theme.palette.secondary.main},
+              borderColor: theme.palette.secondary.main,
+              padding: '8px 30px',
+              paddingRight: '16px',
+              borderRadius: '15px',
+              color: theme.palette.primary.main,
+              justifyContent: 'center'
+            }}
+            onClick = {() => navigate("/maintenance")}
+          >
+            <Typography
+              variant="body3"
+              gutterBottom
+              sx={{
+                color: theme.palette.primary.light
+              }}
+            >
+              Ski
+            </Typography>
+          </Button>
+        </Stack>
+      </Box>
+      <Box
+        sx={{
+          marginTop: '2rem',
+          marginBottom: '2rem'
+        }}>
+        <Typography
+          variant="body3"
+          gutterBottom
+          sx={{
+            color: theme.palette.primary.light
+          }}
+        >
+          I am ...
+        </Typography>
+        <Stack direction="column" spacing={2} sx={{ width: '100%', maxWidth: 360, mt: 3 }}>
+          <Button
+            variant="outlined"
+            fullWidth
+            sx={{
+              height: '7vh',
+              backgroundColor: theme.palette.primary.dark,
+              '&:hover': { backgroundColor: theme.palette.secondary.main},
+              borderColor: theme.palette.secondary.main,
+              padding: '8px 30px',
+              paddingRight: '16px',
+              borderRadius: '15px',
+              color: theme.palette.primary.main,
+              justifyContent: 'center'
+            }}
+            onClick = {() => navigate("/maintenance")}
+          >
+            <Typography
+              variant="body3"
+              gutterBottom
+              sx={{
+                color: theme.palette.primary.light
+              }}
+            >
+              Beginner
+            </Typography>
+          </Button>
+          <Button
+            variant="outlined"
+            fullWidth
+            sx={{
+              height: '7vh',
+              backgroundColor: theme.palette.primary.dark,
+              '&:hover': { backgroundColor: theme.palette.secondary.main},
+              borderColor: theme.palette.secondary.main,
+              padding: '8px 30px',
+              paddingRight: '16px',
+              borderRadius: '15px',
+              color: theme.palette.primary.main,
+              justifyContent: 'center'
+            }}
+            onClick = {() => navigate("/maintenance")}
+          >
+            <Typography
+              variant="body3"
+              gutterBottom
+              sx={{
+                color: theme.palette.primary.light
+              }}
+            >
+              Intermediate
+            </Typography>
+          </Button>
+          <Button
+            variant="outlined"
+            fullWidth
+            sx={{
+              height: '7vh',
+              backgroundColor: theme.palette.primary.dark,
+              '&:hover': { backgroundColor: theme.palette.secondary.main},
+              borderColor: theme.palette.secondary.main,
+              padding: '8px 30px',
+              paddingRight: '16px',
+              borderRadius: '15px',
+              color: theme.palette.primary.main,
+              justifyContent: 'center'
+            }}
+            onClick = {() => navigate("/maintenance")}
+          >
+            <Typography
+              variant="body3"
+              gutterBottom
+              sx={{
+                color: theme.palette.primary.light
+              }}
+            >
+              Advanced
+            </Typography>
+          </Button>
+        </Stack>
+      </Box>
+    </Box>
   );
 };
 
