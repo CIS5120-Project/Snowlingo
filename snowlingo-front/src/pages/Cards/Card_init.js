dfd6246979eea9097aba8d054965f8f86a4a436b
lucawu <lucawu0725@gmail.com>
2024-04-26 15:23:11 -0400

    first card

--- snowlingo-front/src/pages/Cards/Card_init.js
+++ snowlingo-front/src/pages/Cards/Card_init.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './Card_init.css'
 import snow from '../../snowlingo.svg';
-import card_g from './card_g.svg'
+import card_g from './card_g.svg';
+import ski_icon from './ski_icon.svg';
+import jump_1 from './pics/jump_1.svg';
 import { NavLink } from 'react-router-dom';
 // import { Box, Grid, Paper, TextField, Button } from '@mui/material';
-import { Box } from '@mui/material';
+import { Box, Button} from '@mui/material';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 // import { Swiper, SwiperSlide } from 'swiper/react';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -40,6 +42,7 @@ const Card0 = () => {
                 <img id="login-snow" src={snow} alt="logo" style={{ marginTop: "0.5rem", marginBottom: "1.5rem", marginLeft:"5.5rem" }}></img>  
             </Box>
             
+            <img id="ski-icon" src={ski_icon} alt="logo" style={{ marginTop: "0.2rem", marginBottom: "0.5rem", marginLeft:"10.5rem" }}></img>  
             <Swiper
             effect={'coverflow'}
             grabCursor={true}
@@ -67,16 +70,39 @@ const Card0 = () => {
                 // maxWidth: '600px', // Maximum width of the Swiper; adjust as needed
                 // marginLeft: '0px', // Combined with marginRight: 'auto', this centers the Swiper
                 // marginRight: 'auto', 
-                marginTop: '80px',
+                marginTop: '10px',
               }}
             >
-                <SwiperSlide>
+                <SwiperSlide id="first_pic">
                     {/* Content of your first slide */}
                     <div style={{ position: 'relative' }}>
                         <img id="card_backg" src={card_g} alt="logo" />
-                        <button style={{ position: 'absolute', bottom: '20px', left: '20px' }}>
-                        Your Button1
-                        </button>
+                        <img id="jump_1" src={jump_1} alt="logo" 
+                            style={{
+                                position: 'absolute',
+                                top: '35%', // Adjust this value to position `jump_1` as needed
+                                left: '100%', // Adjust this value to position `jump_1` as needed
+                                transform: 'translate(-50%, -50%)', // This centers the image on the container
+                                width: '90%', // Adjust this value to set the size of the `jump_1`
+                                height: 'auto',
+                                zIndex: 2, // Ensure `jump_1` is above `card_backg`
+                            }}
+                        />
+                        <Button color="primary" variant="contained" style={{ position: 'relative', bottom: '150px', left: '105px' }}>
+                        JUMP
+                        </Button>
+                        <p 
+                        style={{ 
+                            position: 'absolute',
+                            top: '67%', // Adjust as needed to move the text up or down
+                            left: '92%', // Center horizontally
+                            transform: 'translate(-50%, -50%)',
+                            width: '170px',
+                            height: 'auto',
+                            color: 'white',
+                            zIndex: 3, // Make sure the text is above the image layers
+                          }}
+                        >Train Focus&Agility</p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
